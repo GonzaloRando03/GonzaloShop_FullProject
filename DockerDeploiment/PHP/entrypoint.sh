338f882bee523c186241b03e6df11abf9b816daf
Gonzalo Rando Serna <103594582+GonzaloRando03@users.noreply.github.com>
2023-05-15 22:26:54 +0200

    Update entrypoint.sh

--- DockerDeploiment/PHP/entrypoint.sh
+++ DockerDeploiment/PHP/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Ejecutamos sleep para que la base de datos mysql tenga tiempo para arrancar y realizar la migración
+sleep 60
+# Realizamos las migraciones e iniciamos el servidor
 cd /var/www/html/
 php artisan migrate
 php artisan octane:start --server="swoole" --host="0.0.0.0" 
